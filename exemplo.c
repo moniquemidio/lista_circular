543cebfd2a1d0d124ad6ca422b5e7886edb6c8bb
Monique Emídio de Oliveira <49343419+moniquemidio@users.noreply.github.com>
2026-05-11 11:29:24 -0300

    Create exemplo.c

---
+++ exemplo.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Estrutura do nó */
+struct lista {
+    int info;
+    struct lista* prox;
+};
+
+typedef struct lista Lista;
+
+/* Inicializa lista vazia */
+Lista* inicializa(void) {
+    return NULL;
+}
+
+/* Inserção no final da lista circular */
+Lista* inserirFinal(Lista* l, int valor) {
+
+    Lista* novo = (Lista*) malloc(sizeof(Lista));
+
+    novo->info = valor;
+
+    /* Procurar último nó */
+    Lista* p = l;
+
+    while (p->prox != l) {
+        p = p->prox;
+    }
+
+    /* Inserção */
+    p->prox = novo;
+    novo->prox = l;
+
+    return l;
+}
+
+/* Impressão da lista circular */
+void imprimir(Lista* l) {
+
+    /* Verifica lista vazia */
+    if (l == NULL) {
+        printf("Lista vazia!\n");
+        return;
+    }
+
+    Lista* p = l;
+
+    /* Percorre a lista */
+    do {
+
+        printf("%d -> ", p->info);
+
+        p = p->prox;
+
+    } while (p != l);
+
+    printf("(inicio)\n");
+}
+
+int main(void) {
+
+    Lista* l;
+
+    /* Inicializa lista */
+    l = inicializa();
+
+    /* Inserções */
+    l = inserirFinal(l, 10);
+    l = inserirFinal(l, 20);
+    l = inserirFinal(l, 30);
+    l = inserirFinal(l, 40);
+
+    /* Impressão */
+    imprimir(l);
+
+    return 0;
+}
